619f662acad5efd9dbf64f008abd33e17600e1f8
Puiling11 <123519805+Puiling11@users.noreply.github.com>
2023-03-20 22:19:51 -0600

    Update map.js

--- map.js
+++ map.js
@@ -9,85 +9,45 @@ L.tileLayer('https://api.mapbox.com/styles/v1/puiling11/clf8xcwjb000001mlrdb8c00
     accessToken: 'pk.eyJ1IjoicHVpbGluZzExIiwiYSI6ImNsZjBkNmllaDAwb3gzcHFqMGdocW5laXUifQ.gd31QkMQxymb2LQ5KR8bOw'
 }).addTo(mymap)
 
-function icons(){
-    const green = '#00FF00'
-
-    const greenMarkerHtmlStyles = `
-    background-color: ${green};
-    position: relative;
-    border-radius: 3rem 3rem 0;
-    transform: rotate(45deg);
-    border: 1px solid #FFFFFF`
-
-    const greenIcon = L.divIcon({
-    className: "my-custom-pin",
-    iconAnchor: [0, 24],
-    labelAnchor: [-6, 0],
-    popupAnchor: [0, -36],
-    html: `<span style="${greenMarkerHtmlStyles}" />`
-    })
-    const blue = '#0000FF'
-
-    const blueMarkerHtmlStyles = `
-    background-color: ${blue};
-    width: 3rem;
-    height: 3rem;
-    display: block;
-    left: -1.5rem;
-    top: -1.5rem;
-    position: relative;
-    border-radius: 3rem 3rem 0;
-    transform: rotate(45deg);
-    border: 1px solid #FFFFFF`
-
-    const blueIcon = L.divIcon({
-    className: "my-custom-pin",
-    iconAnchor: [0, 24],
-    labelAnchor: [-6, 0],
-    popupAnchor: [0, -36],
-    html: `<span style="${blueMarkerHtmlStyles}" />`
-    })
-    const red = '#FF0000'
-
-    const redMarkerHtmlStyles = `
-    background-color: ${red};
-    width: 3rem;
-    height: 3rem;
-    display: block;
-    left: -1.5rem;
-    top: -1.5rem;
-    position: relative;
-    border-radius: 3rem 3rem 0;
-    transform: rotate(45deg);
-    border: 1px solid #FFFFFF`
-
-    const redIcon = L.divIcon({
-    className: "my-custom-pin",
-    iconAnchor: [0, 24],
-    labelAnchor: [-6, 0],
-    popupAnchor: [0, -36],
-    html: `<span style="${redMarkerHtmlStyles}" />`
-    });
-    return [blueIcon, greenIcon, redIcon];
-}
-
 function pinLocation(lat,long,temp){
     
-    const blueIcon = icons()[0];
-    const greenIcon = icons()[1];
-    const redIcon = icons()[2];
+    var blueIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
     if (temp<10) {
-        var marker = L.marker([lat, long], {
-            icon: blueIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long], 
+            {icon: blueIcon}
+            ).addTo(mymap);
     }else if (10<=temp && temp<30){
-        var marker = L.marker([lat, long], {
-            icon: greenIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long], 
+            {icon: greenIcon}
+            ).addTo(mymap);
     }else if(30<=temp){
-        var marker = L.marker([lat, long], {
-            icon: redIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long],
+            {icon: redIcon}
+            ).addTo(mymap);
     }else{
         var marker = L.marker([lat, long]).addTo(mymap);
     }
@@ -100,22 +60,43 @@ function geoJsonLocation(myGeoJson){
     long = myGeoJson['features'][0]['geometry']['coordinates'][0][1];
     temp = myGeoJson['features'][0]['properties']['temp'][0][0];
 
-    
-    const blueIcon = icons()[0];
-    const greenIcon = icons()[1];
-    const redIcon = icons()[2];
+    var blueIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
     if (temp<10) {
-        var marker = L.marker([lat, long], {
-            icon: blueIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long], 
+            {icon: blueIcon}
+            ).addTo(mymap);
     }else if (10<=temp && temp<30){
-        var marker = L.marker([lat, long], {
-            icon: greenIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long], 
+            {icon: greenIcon}
+            ).addTo(mymap);
     }else if(30<=temp){
-        var marker = L.marker([lat, long], {
-            icon: redIcon
-          }).addTo(mymap);
+        var marker = L.marker([lat, long],
+            {icon: redIcon}
+            ).addTo(mymap);
     }else{
         var marker = L.marker([lat, long]).addTo(mymap);
     }
